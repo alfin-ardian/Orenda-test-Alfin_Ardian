52f2295755e230bc6aee1880be503b29e2a9b8f4
alfin ardian <developer.alfin@gmail.com>
2023-05-09 07:57:14 +0700

    initial commit

---
+++ api/models/user.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+
+module.exports = sequelize.define("User", {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+app.get("/users", async (req, res) => {
+  const users = await User.findAll();
+  res.json(users);
 });
 
 module.exports = router;
