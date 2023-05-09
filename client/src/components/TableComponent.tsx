9a5c1e6b93f9a381f26c7a7d5b542c794bc34c26
alfin ardian <developer.alfin@gmail.com>
2023-05-09 20:55:08 +0700

    add tablecomponent

---
+++ client/src/components/TableComponent.tsx
@@ -0,0 +1,214 @@
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TablePagination,
+  Button,
+} from "@material-ui/core";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Autocomplete, { createFilterOptions } from "@mui/material/Autocomplete";
+import { makeStyles } from "@material-ui/core/styles";
+
+const filterOptions = createFilterOptions({
+  matchFrom: "start",
+  stringify: (option: FilmOptionType) => option.title,
+});
+
+interface Column {
+  id: "name" | "code" | "population" | "size" | "density";
+  label: string;
+  minWidth?: number;
+  align?: "right";
+  format?: (value: number) => string;
+}
+
+const columns: readonly Column[] = [
+  { id: "name", label: "Customer Name", minWidth: 170 },
+  { id: "code", label: "Phone Number", minWidth: 100 },
+  {
+    id: "population",
+    label: "Email Address",
+    minWidth: 170,
+    align: "right",
+    format: (value: number) => value.toLocaleString("en-US"),
+  },
+  {
+    id: "size",
+    label: "Address",
+    minWidth: 170,
+    align: "right",
+    format: (value: number) => value.toLocaleString("en-US"),
+  },
+  {
+    id: "density",
+    label: "Action",
+    minWidth: 170,
+    align: "right",
+    format: (value: number) => value.toFixed(2),
+  },
+];
+
+interface FilmOptionType {
+  title: string;
+  year: number;
+}
+interface Data {
+  name: string;
+  code: string;
+  population: number;
+  size: number;
+  density: number;
+}
+
+function createData(
+  name: string,
+  code: string,
+  population: number,
+  size: number
+): Data {
+  const density = population / size;
+  return { name, code, population, size, density };
+}
+
+const rows = [
+  createData("India", "IN", 1324171354, 3287263),
+  createData("China", "CN", 1403500365, 9596961),
+  createData("Italy", "IT", 60483973, 301340),
+  createData("United States", "US", 327167434, 9833520),
+  createData("Canada", "CA", 37602103, 9984670),
+  createData("Australia", "AU", 25475400, 7692024),
+  createData("Germany", "DE", 83019200, 357578),
+  createData("Ireland", "IE", 4857000, 70273),
+  createData("Mexico", "MX", 126577691, 1972550),
+  createData("Japan", "JP", 126317000, 377973),
+  createData("France", "FR", 67022000, 640679),
+  createData("United Kingdom", "GB", 67545757, 242495),
+  createData("Russia", "RU", 146793744, 17098246),
+  createData("Nigeria", "NG", 200962417, 923768),
+  createData("Brazil", "BR", 210147125, 8515767),
+];
+
+const useStyles = makeStyles({
+  header: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: "1rem",
+    padding: "1rem",
+    backgroundColor: "#efefef",
+  },
+});
+const top100Films = [
+  { title: "The Shawshank Redemption", year: 1994 },
+  { title: "The Godfather", year: 1972 },
+];
+
+export default function TableComponent() {
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ width: 1100, overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Stack spacing={2} sx={{ width: 300 }}>
+                  <Autocomplete
+                    freeSolo
+                    id="free-solo-2-demo"
+                    disableClearable
+                    options={top100Films.map((option) => option.title)}
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        label="Search Name"
+                        InputProps={{
+                          ...params.InputProps,
+                          type: "search",
+                        }}
+                      />
+                    )}
+                  />
+                </Stack>
+              </TableCell>
+              <TableCell>
+                <Autocomplete
+                  id="filter-demo"
+                  options={top100Films}
+                  getOptionLabel={(option) => option.title}
+                  filterOptions={filterOptions}
+                  sx={{ width: 300 }}
+                  renderInput={(params) => (
+                    <TextField {...params} label="Filter" />
+                  )}
+                />
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
