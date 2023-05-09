30967d3895fdcb7121ae0b1b00ec0f61b9eddb5c
alfin ardian <developer.alfin@gmail.com>
2023-05-09 20:56:24 +0700

    add redux configuration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import store from "./redux/store.ts";
+import store from "./redux/store.tsx";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
